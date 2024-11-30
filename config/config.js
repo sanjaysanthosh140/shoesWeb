8d9b85ebc7504ee2eded9ed410e678896b2351a6
SanjaySanthosh_Software Developer <137829617+sanjaysanthosh140@users.noreply.github.com>
2024-11-30 16:14:42 +0530

    Update config.js
    "Atlet add on shoes"

--- config/config.js
+++ config/config.js
@@ -2,7 +2,12 @@ const db = require('mongodb').MongoClient
 let  dbname = null
 async function fixadbconnection (){
     try {
-     let  dbdata = await db.connect("mongodb://localhost:27017")
+     let  dbdata = await db.connect("mongodb+srv://sanju:%230ef%23000%2C%23ffff@mernapp.blnm8.mongodb.net/?retryWrites=true&w=majority&appName=mernapp",{
+         useNewUrlParser: true,
+         useUnifiedTopology: true,
+         serverSelectionTimeoutMS: 5000,
+         connectTimeoutMS: 10000
+     })
      if(dbdata){
          dbname = dbdata.db('shoesStore')
         if(!dbname){
